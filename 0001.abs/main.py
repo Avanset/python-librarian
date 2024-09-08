655f3b0e8d875398da8a9a413abae8313014e633
arthur <abstrus415@gmail.com>
2024-09-08 17:23:42 +0900

    0001

---
+++ 0001.abs/main.py
@@ -0,0 +1,27 @@
+
+def main():
+    # abs関数は引数の絶対値を返す
+    print(abs(4))
+    print(abs(-4))
+    
+    # abs関数は引数が浮動小数点数の場合も使える
+    print(abs(4.5))
+    print(abs(-4.5))
+    print(abs(0))
+    
+    # abs関数が定義されたクラスの場合、__abs__関数が呼び出される
+    meter = Meter(7)
+    print(abs(meter))
+    
+# __abs__関数を定義するクラス
+class Meter:
+    
+    def __init__(self, value):
+        self.value = value
+        
+    def __abs__(self):
+        return self.value + 10
+    
+    
+if __name__ == '__main__':
+    main()
